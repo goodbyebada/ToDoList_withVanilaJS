f7db290334860091f8e546da8e62ccdf3e0f1bc4
goodbyebada <goodbyebada@naver.com>
2023-02-18 19:57:49 +0900

    환영메시지 띄우기

---
+++ pra.js
@@ -0,0 +1,47 @@
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
+
+const USER_NAME = username;
+function onLoginsubmit(event) {
+  event.preventDefault();
+  const username = loginInput.value;
+  console.log(USER_NAME);
+
+  localStorage.setItem("username", USER_NAME);
+  //localStorage에 저장
+
+  paintingGreeting(USER_NAME);
+}
+
+function paintingGreeting(username) {
+  loginForm.classList.add("hidden");
+  greeting.classList.remove("hidden");
+  greeting.innerText = `Hello ${username}`;
+
+  //greeting 문구만 보여주기
+  loginForm.classList.add("hidden");
+  //loginform 없애기
+  greeting.classList.remove("hidden");
+  //greeting 보여주기
+  greeting.innerText = `Hello ${USER_NAME}!`;
+}
+
+loginForm.addEventListener("submit", onLoginsubmit);
+//submit하면 onLoginsubmit 실행
+
+const savedUsername = localStorage.getItem("username");
+//localStorage에 저장된 username, savedUsername에 저장
+//새로고침해도 localStorage에 저장되어있음
+
+if (savedUsername == null) {
+  // loginForm 다시 보여주기
+  loginForm.classList.remove("hidden");
+  loginForm.addEventListener("submit", onLoginsubmit);
+} else {
+  //greeting message 띄우기
+  //새로고침하면 모든것은 초기화
+  //기억하는건 savedUsername밖에 없음
+  //--> loginForm도 없애는 코드 추가해줘야함
+  paintingGreeting(savedUsername);
+}
