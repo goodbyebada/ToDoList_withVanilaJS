6fc9b0c9fcd49f61e0303568bab8a08a38603fbd
goodbyebada <goodbyebada@naver.com>
2023-02-19 01:56:16 +0900

    clock/ quote

---
+++ js/clock.js
@@ -0,0 +1,28 @@
+function sayHello() {
+  console.log("hello");
+}
+
+// setInterval(sayHello, 5000);
+// //-> 5초마다 등장
+
+// setTimeout(sayHello, 5000);
+// //-> 5초후에 한번 등장
+
+//setInterval --> 반복 ms 기준
+//setTimeout ms기준
+
+const clock = document.querySelector("h2#clock");
+
+function clockFun() {
+  const date = new Date();
+  const hour = String(date.getHours()).padStart(2, "0");
+  const min = String(date.getMinutes()).padStart(2, "0");
+  const sec = String(date.getSeconds()).padStart(2, "0");
+  clock.innerText = `${hour}:${min}:${sec}`;
+}
+
+clockFun();
+setInterval(clockFun, 1000);
+//1초마다
+
+let phrase = "천사기태는 존재한다!!";
