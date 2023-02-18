e93811cbf3810b341dc015a60575177f4c1d3bcb
goodbyebada <goodbyebada@naver.com>
2023-02-18 19:25:56 +0900

    use localStorage

--- app.js
+++ app.js
@@ -1,39 +1,25 @@
-// const loginForm = document.querySelector("#login-form");
-// const loginInput = document.querySelector("#login-form input");
-// const greeting = document.querySelector("#greeting");
-
-// function onLoginSubmit(event) {
-//   event.preventDefault();
-//   loginForm.classList.add("hidden");
-//   const username = loginInput.value;
-//   console.log(username);
-// }
-
-// loginForm.addEventListener("submit", onLoginSubmit);
-// //addEventListner 함수 직접 실행 x
-
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
-const loginButton = document.querySelector("#login-form button");
 const greeting = document.querySelector("#greeting");
 
-// function onLoginBtnClick() {
-const HIDDEN_CLASSNAME = "hidden";
+function onLoginsubmit(event) {
+  event.preventDefault();
+  const username = loginInput.value;
+  console.log(username);
+  localStorage.setItem("username", username);
+  //localStorage에 저장
 
-function onLoginSubmit(event) {
-  console.log("?");
-  // console.log(loginForm);
-  // event.preventDefault();
-  // loginForm.classList.add(HIDDEN_CLASSNAME);
-  // console.log(loginForm);
-  // const username = loginInput.value;
-  // console.log(username);
-  // greeting.classList.remove(HIDDEN_CLASSNAME);
-  // greeting.innerText = `Hello ${username}`;
-
-  // console.log(greeting);
+  //greeting 문구만 보여주기
+  loginForm.classList.add("hidden");
+  //loginform 없애기
+  greeting.classList.remove("hidden");
+  //greeting 보여주기
+  greeting.innerText = `Hello ${username}!`;
 }
 
-// loginButton.addEventListener("click", onLoginBtnClick);
-loginForm.addEventListener("submit", onLoginSubmit);
-// }
+loginForm.addEventListener("submit", onLoginsubmit);
+//submit하면 onLoginsubmit 실행
+
+const savedUsername = localStorage.getItem("username");
+//localStorage에 저장된 username, savedUsername에 저장
+//새로고침해도 localStorage에 저장되어있음
